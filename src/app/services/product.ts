38d232065498799ff4823be0fa17578d6d8181bf
Artikunazo <artik16032011@gmail.com>
2025-08-06 16:46:43 -0600

    feat: create product service

---
+++ src/app/services/product.ts
@@ -0,0 +1,45 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { IProduct } from '../models/products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  products = signal<IProduct[]>([
+    {
+      name: 'Car Credit',
+      description: 'Car credit description',
+      type: 'credit'
+    },
+    {
+      name: 'House Credit',
+      description: 'Car credit description',
+      type: 'credit'
+    },
+    {
+      name: 'Card Credit',
+      description: 'Car credit description',
+      type: 'credit'
+    },
+    {
+      name: 'Savings',
+      description: 'Savings description',
+      type: 'invsetment'
+    },
+    {
+      name: 'Inversion a plazo',
+      description: 'Car credit description',
+      type: 'investment'
+    },
+    {
+      name: 'Nomina',
+      description: 'Car credit description',
+      type: 'account',
+    },
+    {
+      name: 'Cheques',
+      description: 'Car credit description',
+      type: 'account',
+    },
+  ]).asReadonly();
+}
