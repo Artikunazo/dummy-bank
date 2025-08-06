f78a301a01620194950a20984332492dce1b3bb1
Artikunazo <artik16032011@gmail.com>
2025-08-05 19:22:25 -0600

    feat: add login component

---
+++ src/app/components/login/login.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Header } from '../header/header';
+
+@Component({
+  selector: 'login',
+  imports: [ReactiveFormsModule, Header],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  ngOnInit() {
+    
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
