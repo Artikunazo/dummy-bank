ae9b52f28b9ba5c9c992731a8433c9bdf66ff188
Artikunazo <artik16032011@gmail.com>
2025-08-07 10:55:19 -0600

    feat: add base routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,26 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { ForgotPassword } from './components/forgot-password/forgot-password';
 
-export const routes: Routes = [{
-  path: '',
-  component: Login,
-}, {
-  path: 'forgot-password',
-  component: ForgotPassword,
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPassword,
+  },
+  {
+    path: 'dashboard',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./components/dashboard/dashboard').then(m => m.Dashboard),
+      },
+      {
+        path: 'user-profile',
+        loadChildren: () => import('./components/user-profile/user-profile').then(m => m.UserProfile),
+      }
+    ]
+  }
+];
