e39570d36dc6dab14e17597a61415e36f15113c3
Artikunazo <artik16032011@gmail.com>
2025-08-11 17:05:49 -0600

    feat: improve products layout

--- src/app/dashboard/products/products.ts
+++ src/app/dashboard/products/products.ts
@@ -1,24 +1,24 @@
 import {Component, inject} from '@angular/core';
 import {ProductService} from '../../services/product';
 import {IProduct} from '../../models/products';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
 	selector: 'app-products',
 	standalone: true,
-	imports: [],
+	imports: [CarouselModule],
 	template: `
-		<section class="products-container">
-			@for (product of productsData(); track product.id) {
-				<div class="product">
-					<h3>{{ product.name }}</h3>
-					<p>{{ product.description }}</p>
-					@if (product.accountNumber) {
-						<p>Account Number: {{ product.accountNumber }}</p>
-					}
-
-					<button (click)="signOn(product)">sign On!</button>
-				</div>
-			}
+		<section class="products-container mt-[5rem]">
+      <h4 class="mb-4">Available Products</h4>
+      <p-carousel [value]="productsData()" [numVisible]="1" [numScroll]="1" [circular]="false">
+        <ng-template let-product #item>
+          <div class="product">
+            <h3>{{ product.name }}</h3>
+            <p>{{ product.description }}</p>
+	          <button (click)="signOn(product)">sign On!</button>
+          </div>
+        </ng-template>
+      </p-carousel>
 		</section>
 	`,
 	styleUrl: './products.css',
