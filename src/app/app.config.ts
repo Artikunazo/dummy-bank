b741a36bbbc4e6208ac812ed93be5d76bbd0049d
Artikunazo <artik16032011@gmail.com>
2025-08-14 14:17:23 -0600

    feat: add hash routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
 	provideBrowserGlobalErrorListeners,
 	provideZoneChangeDetection,
 } from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withHashLocation} from '@angular/router';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -15,7 +15,7 @@ export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideZoneChangeDetection({eventCoalescing: true}),
-		provideRouter(routes),
+		provideRouter(routes, withHashLocation()),
 		providePrimeNG({
 			theme: {
 				preset: Aura,
