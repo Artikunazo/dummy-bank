2f219df940d8d5b515a16d580078ee98ce6a62da
Artikunazo <artik16032011@gmail.com>
2025-08-14 18:37:15 -0600

    feat: improve base assets url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseAssetUrl: 'https://artikunazo.github.io/dummy-bank/#/assets/' // Ruta completa para producción
+  baseAssetUrl: 'https://artikunazo.github.io/dummy-bank/assets/' // Ruta completa para producción
 };
